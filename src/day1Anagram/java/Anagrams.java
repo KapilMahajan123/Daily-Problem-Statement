1f1ef1a0d2bd67badd41221fc9afd692e3b917bc
kapilmahajan123 <montyempire123gmail.com>
2022-11-18 18:15:02 +0530

    Added: Checking Anagram or not

---
+++ src/day1Anagram/java/Anagrams.java
@@ -0,0 +1,43 @@
+package day1Anagram.java;
+
+/**
+ * 
+ * @author kapil
+ *
+ */
+
+import java.util.*;
+
+public class Anagrams {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Create two string 
+		String a = "secure";
+		String b ="rescue";
+		
+		//Change to the character array
+		char x[]=a.toCharArray();
+		char y[]=b.toCharArray();
+		
+		//Sorting
+		Arrays.sort(x); //check equal or not
+		Arrays.sort(y);
+		
+		boolean result=Arrays.equals(x,y); //compare to x and y in array equal
+		
+		//if check the condition true or false
+		if(result)
+		{
+			System.out.println("String are Anagram");
+		}
+		else
+		{
+			System.out.println("String are not Anagram");
+
+		}
+
+	}
+
+}
